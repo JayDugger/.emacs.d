f2d2cced93c95cabc577a15cc5704e0c4e38f60f
Jay Dugger <jay.dugger@gmail.com>
2012-05-15 21:36:45 -0500

    20120515--jwd
    UPDATED artbollocks-mode, g-client, package24, & paredit.

--- el-get/g-client/g-loaddefs.el
+++ el-get/g-client/g-loaddefs.el
@@ -631,7 +631,7 @@ Location address. Setting this updates gweb-my-location coordinates  via geocodi
 
 ;;;### (autoloads nil nil ("g-autogen.el" "g-cus-load.el" "g-load-path.el"
 ;;;;;;  "g-utils.el" "g.el" "gnotebook.el" "gwis.el" "indent-files.el"
-;;;;;;  "org2blogger.el") (20401 47547 784776))
+;;;;;;  "org2blogger.el") (20403 1785 500183))
 
 ;;;***
 
