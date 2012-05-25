cb34a131689fa31b232bbf59fb999c1a5d08ee34
Jay Dugger <jay.dugger@gmail.com>
2012-05-25 15:30:23 -0500

    20120525--Jay Dugger

--- el-get/g-client/g-loaddefs.el
+++ el-get/g-client/g-loaddefs.el
@@ -631,7 +631,7 @@ Location address. Setting this updates gweb-my-location coordinates  via geocodi
 
 ;;;### (autoloads nil nil ("g-autogen.el" "g-cus-load.el" "g-load-path.el"
 ;;;;;;  "g-utils.el" "g.el" "gnotebook.el" "gwis.el" "indent-files.el"
-;;;;;;  "org2blogger.el") (20414 23077 321266))
+;;;;;;  "org2blogger.el") (20415 51433 569560))
 
 ;;;***
 
