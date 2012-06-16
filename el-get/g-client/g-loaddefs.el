e90c702807d0f1926af03711dfe97fbc5e88c1e0
Jay Dugger <jay.dugger@gmail.com>
2012-06-16 03:41:38 -0500

    0340 Saturday, 16 June 2012--jwd
    UPDATED paredit, package23, org-mode, g-client, and artboloocks-mode.

--- el-get/g-client/g-loaddefs.el
+++ el-get/g-client/g-loaddefs.el
@@ -631,7 +631,7 @@ Location address. Setting this updates gweb-my-location coordinates  via geocodi
 
 ;;;### (autoloads nil nil ("g-autogen.el" "g-cus-load.el" "g-load-path.el"
 ;;;;;;  "g-utils.el" "g.el" "gnotebook.el" "gwis.el" "indent-files.el"
-;;;;;;  "org2blogger.el") (20442 39063 140875))
+;;;;;;  "org2blogger.el") (20444 16236 933397))
 
 ;;;***
 
