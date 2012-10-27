50bcc7aec9ff64152b0018d1631cb112d7dc46ed
Jay Dugger <jay.dugger@gmail.com>
2012-10-26 23:55:52 -0500

    UPDATED auto-complete, exec-path-from-shell, graphene, projectile

---
+++ elpa/projectile-20121022.1506/projectile-pkg.el
@@ -0,0 +1,1 @@
+(define-package "projectile" "20121022.1506" "Manage and navigate projects in Emacs easily [source: github]" (quote ((s "1.0.0"))))
---
+++ elpa/projectile-20121026.1054/projectile-pkg.el
@@ -0,0 +1,1 @@
+(define-package "projectile" "20121026.1054" "Manage and navigate projects in Emacs easily [source: github]" (quote ((s "1.0.0"))))
