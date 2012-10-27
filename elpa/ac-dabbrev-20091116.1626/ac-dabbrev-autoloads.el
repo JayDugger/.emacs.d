0e014a12e60f469e9b4b03778b2df96e3fe1da36
Jay Dugger <jay.dugger@gmail.com>
2012-10-27 02:00:49 -0500

    Revert "CREATED graphene branch (http://learn.github.com/p/branching.html)"
    This reverts commit 2572bb34ad7366711469b77bb753501c17b3cacf.
    
    Conflicts:
    
    	elpa/auto-complete-20121022.2254/auto-complete-autoloads.el
    	elpa/auto-complete-20121022.2254/auto-complete-config.el
    	elpa/auto-complete-20121022.2254/auto-complete-config.elc
    	elpa/auto-complete-20121022.2254/auto-complete-pkg.elc
    	elpa/auto-complete-20121022.2254/auto-complete.el
    	elpa/auto-complete-20121022.2254/auto-complete.elc
    	elpa/auto-complete-20121022.2254/dict/ada-mode
    	elpa/auto-complete-20121022.2254/dict/c++-mode
    	elpa/auto-complete-20121022.2254/dict/c-mode
    	elpa/auto-complete-20121022.2254/dict/caml-mode
    	elpa/auto-complete-20121022.2254/dict/clojure-mode
    	elpa/auto-complete-20121022.2254/dict/clojurescript-mode
    	elpa/auto-complete-20121022.2254/dict/coq-mode
    	elpa/auto-complete-20121022.2254/dict/css-mode
    	elpa/auto-complete-20121022.2254/dict/erlang-mode
    	elpa/auto-complete-20121022.2254/dict/go-mode
    	elpa/auto-complete-20121022.2254/dict/haskell-mode
    	elpa/auto-complete-20121022.2254/dict/java-mode
    	elpa/auto-complete-20121022.2254/dict/javascript-mode
    	elpa/auto-complete-20121022.2254/dict/lua-mode
    	elpa/auto-complete-20121022.2254/dict/php-mode
    	elpa/auto-complete-20121022.2254/dict/python-mode
    	elpa/auto-complete-20121022.2254/dict/ruby-mode
    	elpa/auto-complete-20121022.2254/dict/scheme-mode
    	elpa/auto-complete-20121022.2254/dict/sclang-mode
    	elpa/auto-complete-20121022.2254/dict/sh-mode
    	elpa/auto-complete-20121022.2254/dict/tcl-mode
    	elpa/auto-complete-20121022.2254/dict/ts-mode
    	elpa/auto-complete-20121022.2254/dict/tuareg-mode
    	elpa/graphene-20121022.957/graphene-autoloads.el
    	elpa/graphene-20121022.957/graphene-editing.el
    	elpa/graphene-20121022.957/graphene-editing.elc
    	elpa/graphene-20121022.957/graphene-env.el
    	elpa/graphene-20121022.957/graphene-env.elc
    	elpa/graphene-20121022.957/graphene-helper-functions.el
    	elpa/graphene-20121022.957/graphene-helper-functions.elc
    	elpa/graphene-20121022.957/graphene-keys.el
    	elpa/graphene-20121022.957/graphene-keys.elc
    	elpa/graphene-20121022.957/graphene-linux-defaults.el
    	elpa/graphene-20121022.957/graphene-look.el
    	elpa/graphene-20121022.957/graphene-osx-defaults.el
    	elpa/graphene-20121022.957/graphene-other-defaults.el
    	elpa/graphene-20121022.957/graphene-pkg.el
    	elpa/graphene-20121022.957/graphene-projects.el
    	elpa/graphene-20121022.957/graphene-projects.elc
    	elpa/graphene-20121022.957/graphene-speedbar.el
    	elpa/graphene-20121022.957/graphene-speedbar.elc
    	elpa/graphene-20121022.957/graphene-theme.el
    	elpa/graphene-20121022.957/graphene-windows-defaults.el
    	elpa/graphene-20121022.957/graphene-windows-defaults.elc
    	elpa/graphene-20121022.957/graphene.el
    	elpa/popup-20121020.1203/popup-pkg.elc
    	elpa/projectile-20121026.1054/projectile-autoloads.el
    	elpa/projectile-20121026.1054/projectile-pkg.el
    	elpa/projectile-20121026.1054/projectile-pkg.elc
    	elpa/projectile-20121026.1054/projectile.el
    	elpa/projectile-20121026.1054/projectile.elc

--- elpa/ac-dabbrev-20091116.1626/ac-dabbrev-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; ac-dabbrev-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("ac-dabbrev-pkg.el" "ac-dabbrev.el") (20612
-;;;;;;  46858 567295 613000))
-
-;;;***
-
-(provide 'ac-dabbrev-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ac-dabbrev-autoloads.el ends here
