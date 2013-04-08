030b4ccaea11a922cd8f231eadaa08c7a19f2aaf
Jay Dugger <jay.dugger@gmail.com>
2013-04-08 02:13:15 -0500

    UPDATED anti-zenburn-theme, haskell-mode, paredit-mode, solarized-theme

--- elpa/anti-zenburn-theme-20130319.2353/anti-zenburn-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "anti-zenburn-theme" "20130319.2353" "Low-contrast Zenburn-inverted theme [source: github]" (quote nil))
---
+++ elpa/solarized-theme-20130407.1956/solarized-theme-pkg.el
@@ -0,0 +1,2 @@
+(define-package "solarized-theme" "20130407.1956" "The Solarized color theme, ported to Emacs. [source: github]" 'nil)
+
