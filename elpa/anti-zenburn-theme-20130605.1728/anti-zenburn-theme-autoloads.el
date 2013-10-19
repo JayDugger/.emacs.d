780db861236e722fc1520cc0b435c7442924cb22
Jay Dugger <jay.dugger@gmail.com>
2013-10-19 02:32:42 -0500

    20131019--why is $EDITOR nano?

--- elpa/anti-zenburn-theme-20130605.1728/anti-zenburn-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; anti-zenburn-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "anti-zenburn-theme" "anti-zenburn-theme.el"
-;;;;;;  (20913 16519))
-;;; Generated autoloads from anti-zenburn-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("anti-zenburn-theme-pkg.el") (20913 16519
-;;;;;;  443596))
-
-;;;***
-
-(provide 'anti-zenburn-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; anti-zenburn-theme-autoloads.el ends here
