1ca9cba92d9b91d5050d8b3713bcff0593bb54d6
Jay Dugger <jay.dugger@gmail.com>
2014-03-01 05:21:48 -0600

    UPDATED minimap

---
+++ elpa/minimap-1.1/minimap-autoloads.el
@@ -0,0 +1,33 @@
+;;; minimap-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (minimap-kill minimap-create) "minimap" "minimap.el"
+;;;;;;  (21265 39927))
+;;; Generated autoloads from minimap.el
+
+(autoload 'minimap-create "minimap" "\
+Create a minimap sidebar.
+
+\(fn)" t nil)
+
+(autoload 'minimap-kill "minimap" "\
+Kill minimap.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("minimap-pkg.el") (21265 39927 133310))
+
+;;;***
+
+(provide 'minimap-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; minimap-autoloads.el ends here
---
+++ elpa/minimap-1.1/minimap-pkg.el
@@ -0,0 +1,1 @@
+(define-package "minimap" "1.1" "Sidebar showing a \"mini-map\" of a buffer" (quote nil))
