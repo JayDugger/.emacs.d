a28d128772e2b3b4c7d68efa392bf119803ecbb2
Jay Dugger <jay.dugger@gmail.com>
2014-05-19 03:29:42 -0500

    ADDED eprime-mode (read source)

---
+++ elpa/eprime-mode-1.1.2/eprime-mode-autoloads.el
@@ -0,0 +1,41 @@
+;;; eprime-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "eprime-mode" "eprime-mode.el" (21363 34802
+;;;;;;  0 0))
+;;; Generated autoloads from eprime-mode.el
+
+(autoload 'eprime-check-buffer "eprime-mode" "\
+Checks the current buffer for banned words and applies a face
+   to them.
+
+\(fn)" t nil)
+
+(autoload 'eprime-check-word "eprime-mode" "\
+Checks the word that's currently entering.
+
+\(fn)" t nil)
+
+(autoload 'eprime-remove-corrections "eprime-mode" "\
+Removes the overlayed corrections on words.
+
+\(fn)" t nil)
+
+(autoload 'eprime-mode "eprime-mode" "\
+Minor mode for checking text conforms to E'. Change eprime-banned-words-face
+  to change what banned words look like, and use (setq eprime-ignore-case nil) if you
+  do not want it to match upper case words.
+  (eprime-check-buffer), when invoked, can check a buffer without turning the mode on.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; eprime-mode-autoloads.el ends here
---
+++ elpa/eprime-mode-1.1.2/eprime-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "eprime-mode" "1.1.2" "An E-prime checking mode for Emacs" 'nil)
