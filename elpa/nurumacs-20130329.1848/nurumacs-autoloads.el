c89e52f11d165b4c1684068800b7d7da3e27815e
Jay Dugger <jay.dugger@gmail.com>
2017-06-09 04:28:43 -0500

    REMOVED hyperbole, neotree, and nurumacs

--- elpa/nurumacs-20130329.1848/nurumacs-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; nurumacs-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("nurumacs-pkg.el" "nurumacs.el") (20821
-;;;;;;  28732 477328))
-
-;;;***
-
-(provide 'nurumacs-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; nurumacs-autoloads.el ends here
--- elpa/nurumacs-20130329.1848/nurumacs-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "nurumacs" "20130329.1848" "smooth-scrolling and minimap, like sublime editor [source: github]" (quote nil))
